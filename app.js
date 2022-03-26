8830c48a92b042f37a0b8dc74ee131fb9f23c5c1
Aleksander <aleksandergorecki80@gmail.com>
2022-03-26 21:06:42 +0000

    Updates

--- app.js
+++
@@ -1,6 +0,0 @@
-import { BookInput } from './components/book-input';
-import { BookList } from './components/book-list';
-import './style.scss';
-new BookInput();
-export const bookList = new BookList('All titles');
-//# sourceMappingURL=app.js.map
